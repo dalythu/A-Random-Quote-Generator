348094c84b9cb19bd2112bf76812fadcfe14e4d3
Thurston Daly <dalythu@gmail.com>
2021-06-10 20:09:07 -0400

    Added auto refresh and random background functionality

--- js/script.js
+++ js/script.js
@@ -10,86 +10,75 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-let quotes = [
-  {
-    quote:"I got mind control over Debo. When he here, I be quiet, but when he leave, I be talkin' again.",
-    source:"Smokey",
-    citation:"Friday",
-    year: 1995,
-    tags: ["movie quotes", "humor"]
-  },
-  {
-    quote:"Hello boys! I'm baaack!",
-    source:"Russell Casse",
-    citation:"Indepedence Day",
-    year:1996,
-    tags: ["movie quotes"]
-
-  }, 
-  {
-    quote:"Here's to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the  square holes. The ones who see things differently. They're not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. About the only thing you can't do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world, are the ones who do.",
-    source:"Rob Siltanen"
-  },
-  {
-    quote:"When one door of happiness closes, another opens, but often we look so long at the closed door that we do not see the one that has been opened for us.",
-    source:"Helen Keller"
-  },
-  {
-    quote:"The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.",
-    source:"Winston Churchill"
-  }
-];
+let quotes = [{
+	quote: "I got mind control over Debo. When he here, I be quiet, but when he leave, I be talkin' again.",
+	source: "Smokey",
+	citation: "Friday",
+	year: 1995,
+	tags: ["movie quotes", "humor"]
+}, {
+	quote: "Hello boys! I'm baaack!",
+	source: "Russell Casse",
+	citation: "Indepedence Day",
+	year: 1996,
+	tags: ["movie quotes"]
+}, {
+	quote: "Here's to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the  square holes. The ones who see things differently. They're not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. About the only thing you can't do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world, are the ones who do.",
+	source: "Rob Siltanen"
+}, {
+	quote: "When one door of happiness closes, another opens, but often we look so long at the closed door that we do not see the one that has been opened for us.",
+	source: "Helen Keller"
+}, {
+	quote: "The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.",
+	source: "Winston Churchill"
+}];
 
 
 /***
  * `getRandomQuote` function
 ***/
-function getRandomQuote(){
-  let randomNumber = Math.floor( Math.random() * quotes.length);
-  let selectedQuote = quotes[randomNumber];
-  // let quoteObject = {};
-  // quoteObject.quote += selectedQuote.quote;
-  // quoteObject.source += selectedQuote.source;
-  // if (!selectedQuote.citation){
-  //   quoteObject = quoteObject;
-  // } else {
-  //   quoteObject.citation += selectedQuote.citation
-  // };
-  // if (!selectedQuote.year){
-  //   quoteObject = quoteObject;
-  // } else {
-  //   quoteObject.year += selectedQuote.year
-  // };
-  // console.log(selectedQuote);
-  // console.log(quoteObject);
-  return selectedQuote;
+function getRandomQuote() {
+	let randomNumber = Math.floor(Math.random() * quotes.length);
+	let selectedQuote = quotes[randomNumber];
+	return selectedQuote;
 };
 
+function changeBackground() {
+  let value1 = Math.floor(Math.random() * 256)+1;
+  let value2 = Math.floor(Math.random() * 256)+1;
+  let value3 = Math.floor(Math.random() * 256)+1;
+  document.body.style.background = `rgb(${value1},${value2},${value3}) `;
+}
+
+setInterval(printQuote, 5000);
 
 /***
  * `printQuote` function
 ***/
-function printQuote(){
-  let selectedQuote = getRandomQuote();
-  let html = '';
-  html += `<p class="quote">${selectedQuote.quote}</p>
+
+function printQuote() {
+  changeBackground();
+	let selectedQuote = getRandomQuote();
+	let html = '';
+	html += `<p class="quote">${selectedQuote.quote}</p>
 <p class="source"> ${selectedQuote.source}`;
-  if (!selectedQuote.citation){
-    html = html;
-  } else {
-    html += `
+	if (!selectedQuote.citation) {
+		html = html;
+	} else {
+		html += `
     <span class="citation">${selectedQuote.citation}</span>`
-  };
-  if (!selectedQuote.year){
-    html = html;
-  } else {
-    html += `
+	};
+	if (!selectedQuote.year) {
+		html = html;
+	} else {
+		html += `
     <span class="year">${selectedQuote.year}</span>`
-  };
-  html += `
+	};
+	html += `
 </p>`;
-return document.getElementById('quote-box').innerHTML = html;
-}
+	return document.getElementById('quote-box').innerHTML = html;
+};
+
 
 /***
  * click event listener for the print quote button
